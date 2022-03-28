f62031e75c0aeb28389b733d5f7c1a7d207c7785
Colin Brooks <mcbrooks@pm.me>
2022-03-27 21:51:28 -0400

    added rudimentary score keeping and timer adj for card answering

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 var beginBtn = document.querySelector(".btn")
-var secondsLeft = 60
 var timerEl = document.querySelector('.timer')
+var scoreEl = document.querySelector('.score')
 var cardHeadEl = document.querySelector('.card-header')
 var cardBodyEl = document.querySelector('.card-body')
 var answerBoxEl = document.querySelector('.answer-boxes')
 
+var secondsLeft = 60
 var score = 0
 var cardIndex = []
 var cardQuestion = ""
@@ -12,9 +13,9 @@ var cardAnswers = []
 var highScoreIndex = []
 
 cardIndex = [
-    ["This is a test question", ["test answer 1", "test answer 2", "test answer 3", "test answer 4"]],
-    ["This is another test question", ["test answer 5", "test answer 6", "test answer 7", "test answer 8"]],
-    ["This is a 3rd test question", ["test answer 9", "test answer 10", "test answer 11", "test answer 12"]],
+    ["This is a test question", ["test answer 1", "test answer 2", "test answer 3", "test answer 4"], "test answer 1"],
+    ["This is another test question", ["test answer 5", "test answer 6", "test answer 7", "test answer 8"], "test answer 5"],
+    ["This is a 3rd test question", ["test answer 9", "test answer 10", "test answer 11", "test answer 12"], "test answer 9"],
 ]
 
 function init() {
@@ -26,6 +27,7 @@ function init() {
 }
 
 function resetTimer() {
+    clearInterval(timerInterval)
     secondsLeft = 60
     var timerInterval = setInterval(function() {
         secondsLeft--;
@@ -40,6 +42,7 @@ function resetTimer() {
 
 function resetScore() {
     score = 0
+    scoreEl.textContent = ("Score: " + score)
 }
 
 function resetCard() {
@@ -58,27 +61,38 @@ function shuffleArray(arg1) {
 
 function drawCard() {
     cardQuestion = cardIndex[0][0]
-    cardAnswers = cardIndex[0][1]
-    shuffleArray(cardAnswers)
+    cardPossibleAnswers = cardIndex[0][1]
+    cardAnswer = cardIndex[0][2]
+    shuffleArray(cardPossibleAnswers)
     cardHeadEl.textContent = cardQuestion
-
-    var answer0btn = document.createElement('button')
-    answer0btn.innerText = cardAnswers[0]
-
-    var answer1btn = document.createElement('button')
-    answer1btn.innerText = cardAnswers[1]
-
-    var answer2btn = document.createElement('button')
-    answer2btn.innerText = cardAnswers[2]
-
-    var answer3btn = document.createElement('button')
-    answer3btn.innerText = cardAnswers[3]
     
-    for(let i = 0; i < cardAnswers.length; i++) {
+    for(let i = 0; i < cardPossibleAnswers.length; i++) {
+        window['answerBtn'+i] = document.createElement('button') 
+        window['answerBtn'+i].innerText = cardPossibleAnswers[i]
+        window['answerBtn'+i].addEventListener('click', function() {
+            if(eval('answerBtn'+i).innerText == cardAnswer) {
+                console.log("YEET")
+                score += 5
+                scoreEl.textContent = ("Score: " + score)
+            }
+            else {
+                secondsLeft -= 5
+                if(score > 0) {
+                    score -= 5
+                    scoreEl.textContent = ("Score: " + score)
+                }
+            }
+        }
+        )
         var answerEl = document.createElement('li')
-        answerEl.appendChild(eval('answer'+i+'btn'))
+        answerEl.appendChild(window['answerBtn'+i])
         answerBoxEl.appendChild(answerEl)
     } 
 }
 
+function checkIfRight() {
+
+}
+
 beginBtn.addEventListener("click", init)
+
